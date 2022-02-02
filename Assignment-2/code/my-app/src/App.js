df48604ecc438eb209edbeb8fe6bba525e0fb5b5
meghna singh <megsingh2212@gmail.com>
2022-02-02 13:49:24 +0530

    assignment 2 made changes and ran test successfully

--- Assignment-2/code/my-app/src/App.js
+++ Assignment-2/code/my-app/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 
 
@@ -8,7 +8,17 @@ function App(props) {
     
 return(
   <div className="container">
-  {/* Start your React code here */}
+  <img src={employee.profileImg} alt='cannot display profile image'/>
+  <div className='info'>
+  
+  <h1>{employee.name}</h1>
+  <h5>Location</h5>
+  <p><strong>{employee.location}</strong></p>
+  <h5>Blood Group</h5>
+  <p><strong>{employee.bloodGroup}</strong></p>
+  <h5>Age</h5>
+  <p><strong>{employee.age}</strong></p>
+  </div>
 </div>
 )
 }
